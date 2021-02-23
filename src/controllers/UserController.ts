1c6b6c0799c4defba0e3f2af98dc5b31e7e42a20
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-02-23 16:22:24 -0300

    build: create and test user insert on database

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,17 +1,30 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 
 class UserController {
   async create(request: Request, response: Response) {
 
-    const body = request.body;
+    const { name, email } = request.body;
 
-    console.log(body);
+    const userRepository = getRepository(User);
 
+    const userAlreadyExists = await userRepository.findOne({
+      email
+    })
 
+    if(userAlreadyExists) {
+      return response.status(400).json({
+        error: "User already exists!"
+      })
+    }
 
+    const user = userRepository.create({ name, email })
 
-    response.json({});
+    await userRepository.save(user);
+
+    return response.status(201).json(user);
   }
 };
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -14,7 +14,7 @@ class User {
   email: string;
 
   @CreateDateColumn()
-  crated_at: Date;
+  created_at: Date;
 
   constructor() {
     if(!this.id) {
