11a8781ae6409e80825e921cfea52e2b395d715c
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-02-26 11:18:21 -0300

    build: create cmv surveyuser table

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveyController } from './controllers/SurveyController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router()
 
 const userController = new UserController();
 const surveysController = new SurveyController();
+const sendMailController = new SendMailController();
 
 router.post('/users', userController.create);
 
 router.get('/surveys', surveysController.show);
 router.post('/surveys', surveysController.create);
+
+router.post('/sendMail', sendMailController.execute);
 
 export { router };
